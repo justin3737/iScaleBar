5ec7a8a1583a6c1989d0a56c37ecb32b2a975d5b
justin3737 <juns3700@gmail.com>
2013-06-06 14:31:12 +0800

    Update iScaleBar.js
    加入動態讀取 CSS/JS File. function

--- assets/iScaleBar.js
+++ assets/iScaleBar.js
@@ -1,5 +1,5 @@
 /** 
- * Version: 0.1.0 (2013/06/05)
+ * Version: 1.0.1 (2013/06/06)
  */
 var iScaleBar = function ( MapCanvas, mapObj, options){ 
 	var bgStyle = { "background" : 'url(' + options.imgUrl + ') no-repeat'};
@@ -7,11 +7,12 @@ var iScaleBar = function ( MapCanvas, mapObj, options){
 	var _object  = "";
 	var _elm = {};
 	var _data ={ 
+    mapCanvas : MapCanvas,
 		mapScaleVal : -1 ,
 		mapZoomMax : ( options.zoom_gap_max ) ? options.zoom_gap_max : 12,
 		mapZoomMin : ( options.zoom_gap_min ) ? options.zoom_gap_min : 1,
 		useMouseWheel: ( options.useMouseWheel ) ? options.useMouseWheel : true,
-		mouseWheelPluginUrl : ( options.mouseWheelPluginUrl ) ? options.mouseWheelPluginUrl : "assets/jquery.mousewheel.js"
+		mouseWheelPluginUrl : ( options.mouseWheelPluginUrl ) ? options.mouseWheelPluginUrl : "http://hfcdn.housefun.com.tw/iScaleBar/jquery.mousewheel.js"
 	};
 
 	var map = mapObj;
@@ -65,13 +66,55 @@ var iScaleBar = function ( MapCanvas, mapObj, options){
 
     function init(){
     	if(_data.useMouseWheel) {
-    		var includeStr = '<script type="text/javascript" src="'+ _data.mouseWheelPluginUrl +'"></script>';
-    		$('head').append(includeStr);
-    	}
+        loadjscssfile( _data.mouseWheelPluginUrl, "js", function(){
+          // callback
+          $("#" + _data.mapCanvas ).mousewheel(function (event, delta) {
+            var val = getZoomVal();
+            (delta >= 0) ? (( val  < _data.mapZoomMax ) ? val  += 1 : val ) : (( val > _data.mapZoomMin ) ? val -= 1 : val);
+            if (val >= 1 && val <= 12) { 
+              map.ZoomTo( val );
+              setScalebarPos( val ); 
+            }
+            return false;
+          });
+
+        });
+    	};
+
     	var val = getZoomVal();
       val = (val < _data.mapZoomMin) ? _data.mapZoomMin : val;
       val = (val > _data.mapZoomMax) ? _data.mapZoomMax : val;
     	setScalebarPos(val);
+    };
+
+    // 動態讀取 CSS/JS File.
+    function loadjscssfile (filename, filetype, callback){
+      //if filename is a external JavaScript file
+      if (filetype === "js"){ 
+        var fileref = document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src", filename);
+        if (fileref.readyState) { //IE
+         fileref.onreadystatechange = function () {
+            if (fileref.readyState == "loaded" || fileref.readyState == "complete") {
+              fileref.onreadystatechange = null;
+              callback();
+            }
+          };
+        } else { 
+        // Other Browsers.
+          fileref.onload = function () { callback(); };
+        }
+      } else if (filetype === "css"){ 
+        var fileref = document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+      }
+
+      if (typeof fileref !== "undefined"){
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+      }
     };
 
     function getZoomVal(){
@@ -121,19 +164,7 @@ var iScaleBar = function ( MapCanvas, mapObj, options){
     		scaleControl(evt.pageY - scaleTop);
    		});
 	   };
-
-    if(_data.useMouseWheel){
-        $("#" + MapCanvas).mousewheel(function (event, delta) {
-        var val = getZoomVal();
-        (delta >= 0) ? (( val  < _data.mapZoomMax ) ? val  += 1 : val ) : (( val > _data.mapZoomMin ) ? val -= 1 : val);
-        if (val >= 1 && val <= 12) { 
-          map.ZoomTo( val );
-          setScalebarPos( val ); 
-        }
-        return false;
-      });
-    };
-
+     
     _elm.zoomInBtn.bind('click',function(evt){
     	evt.stopPropagation();
     	scaleControl("in");
